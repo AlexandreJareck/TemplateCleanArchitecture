31e2e1a38203ec1a314096964876796e7e00a386
AlexandreJareck <alexandrejareck@gmail.com>
2025-03-11 21:58:44 -0300

    test: add tests integration

---
+++ Tests/Template.IntegrationTests/Data/BaseEfRepoTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Template.Application.Interfaces;
+using Template.Infrastructure.Persistence.Contexts;
+using Template.Infrastructure.Persistence.Repositories;
+
+namespace Template.IntegrationTests.Data;
+
+public abstract class BaseEfRepoTestFixture
+{
+    protected ApplicationDbContext dbContext;
+
+    protected BaseEfRepoTestFixture()
+    {
+        var options = CreateNewContextOptions();
+        IAuthenticatedUserService authenticatedUserService = new AuthenticatedUserService(Guid.NewGuid().ToString(), "UserName");
+
+        dbContext = new ApplicationDbContext(options, authenticatedUserService);
+    }
+
+    protected static DbContextOptions<ApplicationDbContext> CreateNewContextOptions()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkInMemoryDatabase()
+            .BuildServiceProvider();
+
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        builder.UseInMemoryDatabase(nameof(ApplicationDbContext))
+               .UseInternalServiceProvider(serviceProvider);
+
+        return builder.Options;
+    }
+
+    protected GenericRepository<T> GetRepository<T>() where T : class
+    {
+        return new GenericRepository<T>(dbContext);
+    }
+
+    protected IUnitOfWork GetUnitOfWork()
+    {
+        return new UnitOfWork(dbContext);
+    }
+}
+internal record AuthenticatedUserService(string UserId, string UserName) : IAuthenticatedUserService;
---
+++ Tests/Template.IntegrationTests/Data/EfRepositoryAdd.cs
@@ -0,0 +1,49 @@
+﻿using Shouldly;
+using Template.Domain.Products.Entities;
+
+namespace Template.IntegrationTests.Data;
+public class EfRepositoryAdd : BaseEfRepoTestFixture
+{
+    [Fact]
+    public async Task AddProduct_ShouldAddProductSuccessfully()
+    {
+        // Arrange
+        var productName = "Test Product";
+        var productPrice = 1000;
+        var productBarCode = "123456789";
+        var repository = GetRepository<ProductEntity>();
+        var unitOfWork = GetUnitOfWork();
+        var product = new ProductEntity(productName, productPrice, productBarCode);
+
+        // Act
+        await repository.AddAsync(product);
+        await unitOfWork.SaveChangesAsync();
+
+        // Assert
+        var newProduct = (await repository.GetAllAsync()).FirstOrDefault();
+
+        newProduct.ShouldNotBeNull();
+        newProduct.Name.ShouldBe(productName);
+        newProduct.Price.ShouldBe(productPrice);
+        newProduct.BarCode.ShouldBe(productBarCode);
+    }
+
+    [Fact]
+    public async Task AddProductWithoutSaving_ShouldNotPersistProduct()
+    {
+        // Arrange
+        var productName = "Test Product";
+        var productPrice = 1000;
+        var productBarCode = "123456789";
+        var repository = GetRepository<ProductEntity>();
+        var product = new ProductEntity(productName, productPrice, productBarCode);
+
+        // Act
+        await repository.AddAsync(product);
+
+        // Assert
+        var newProduct = (await repository.GetAllAsync()).FirstOrDefault();
+
+        newProduct.ShouldBeNull();
+    }
+}
